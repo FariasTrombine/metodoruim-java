95bf42d6740f567d8fd1b3aa2d2adbad02317992
Guilherme Trombini <guilhermetrombini@hotmail.com>
2026-02-23 20:43:00 -0300

    bad solution

---
+++ src/Application/Program.java
@@ -0,0 +1,39 @@
+package Application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    static void main(String[] args) {
+        method1();
+        System.out.println("End of program");
+
+    }
+    public static void method1(){
+        System.out.println("***METHOD1 START***");
+        method2();
+        System.out.println("***METHOD1 END***");
+
+    }
+    public static void method2(){
+       System.out.println("***METHOD2 START***");
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Inválid Position");
+            e.printStackTrace(); //ira mostrar tod a chamada  de métodos que acarretou naquela exceção
+            sc.next();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Input Error");
+        }
+
+    sc.close();
+    }
+
+}
